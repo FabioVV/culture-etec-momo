cc9e8e740e4e84ca137e601ba4a75893e24a90a0
Fábio Varela <fabio-varela@protonmail.com>
2023-04-26 16:02:07 -0300

    changed folder structure

--- static/flash_msg.js
+++
@@ -1,8 +0,0 @@
-let a = document.querySelector('#msg')
-
-setTimeout(() =>{
-    a.classList.add('removed')
-    a.addEventListener('transitionend',() => {
-        a.remove()
-    })
-}, 1500)
---
+++ static/js/main.js
@@ -0,0 +1,27 @@
+let a = document.querySelector('#msg')
+
+setTimeout(() =>{
+    a.classList.add('removed')
+    a.addEventListener('transitionend',() => {
+        a.remove()
+    })
+}, 1500)
+
+
+
+function setActive(){
+    
+    let inactiveB = document.querySelectorAll('.inactive')
+
+
+    for(let i = 0; i < inactiveB.length; i++){
+    if(document.location.href.indexOf(inactiveB[i].href) >= 0){
+            inactiveB[i].classList.add('active')
+    }
+    
+    }
+
+}
+
+
+window.onload = setActive
